800394b56e2e1d0d7951e14d0fdc80ab27499b50
Finlay Johnston <finlayjohnston60@gmail.com>
2015-10-19 15:24:37 +0100

    hello world

--- index.php
+++ index.php
@@ -14,9 +14,16 @@
 $myname = "Frodo Baggins";
 $myage = 111;
 echo "My name is" . $myname . "and I am " . $myage;
-echo "Hello World.";
-echo "Hello," . " " . "world" . "!";
-echo 5 * 7;
+//echo "Hello World.";
+//echo "Hello," . " " . "world" . "!";
+//echo 5 * 7;
+$name = "Edgar";
+if($name == "Simon"){
+    print "I know you!";
+}
+else{
+    print "who are you?";
+}
 ?>
 </p>
 </body>
