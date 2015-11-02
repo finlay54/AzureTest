7e281cc9658647484312f1f6cb9283eacb452d75
Finlay Johnston <finlayjohnston60@gmail.com>
2015-11-02 13:55:22 +0000

    Uploading the cookies lab to azure

---
+++ cookies.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1202829
+ * Date: 02/11/2015
+ * Time: 13:29
+ */
+$username = "user1";
+$password = "password1";
+
+setcooke('access_level', 'standarduser');
+
+?>
+</p>
+</body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1202829
+ * Date: 02/11/2015
+ * Time: 13:49
+ */
+function displayAccessLevelInformation($accessLevel){
+    if($accessLevel == 'standarduser'){
+        echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($accessLevel == "root"){
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
+
+?>
+    </p>
+</body>
+</html>
+
